22dbbf5c8e3fd669c5cec00c1b0c321643f7271a
Yasser Carreón <yacasia@gmail.com>
2018-12-07 10:30:50 -0600

    Webpack and project config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,44 @@
-const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve('awesome-typescript-loader'),
-  });
-  defaultConfig.plugins.push(new TSDocgenPlugin());
-  defaultConfig.resolve.extensions.push('.ts', '.tsx');
-  return defaultConfig;
+const CSSModuleLoader = {
+  loader: 'css-loader',
+  options: {
+    modules: true,
+    minimize: true,
+    sourceMap: true,
+    localIdentName: '[local]__[hash:base64:5]',
+  },
+};
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.module\.scss$/,
+            include: resolveApp('src'),
+            loaders: [
+              require.resolve('style-loader'),
+              CSSModuleLoader,
+              require.resolve('sass-loader'),
+            ],
+          },
+          {
+            test: /\.scss$/,
+            include: resolveApp('src'),
+            loaders: [
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
+              require.resolve('sass-loader'),
+            ],
+          },
+        ],
+      },
+    ],
+  },
 };
